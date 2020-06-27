9f66e11f5245edcc4612c22f8562277f733f4683
Eduard Benedic <benediceduard@outlook.com>
2020-06-27 19:41:09 +0100

    pacjage

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = () => ({
     default: {
       connector: 'mongoose',
       settings: {
-          "uri": `mongodb+srv://${mongoDB_name}:${process.env.MongoDB_password}@cluster0-snmef.mongodb.net/${process.env.MongoDB_database}?retryWrites=true&w=majority`
+          "uri": `${process.env.MongoDB_URI}`
       },
       options: {
         ssl: true
